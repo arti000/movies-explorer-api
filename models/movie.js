37d3b6e368357ce3871854286dbb0a3c233add0e
Artem Kireev <al-mysri@yandex.ru>
2022-08-01 22:01:52 +0300

    feat: add movie scheme

---
+++ models/movie.js
@@ -0,0 +1,95 @@
+// ----------------------------------------------------------------------------
+//                            Схема модели фильма
+// ----------------------------------------------------------------------------
+
+// Подключаем ODM - Mongoose
+const mongoose = require('mongoose');
+
+// Импортируем валидатор в проект
+const validator = require('validator');
+
+// Создаем схему фильма
+const movieSchema = new mongoose.Schema({
+  country: {
+    type: String,
+    required: true,
+  },
+  director: {
+    type: String,
+    required: true,
+  },
+  duration: {
+    type: Number,
+    required: true,
+  },
+  year: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  image: {
+    type: String,
+    required: true,
+    validate: {
+      validator(image) {
+        return validator.isURL(image);
+      },
+      message: 'Неверно указана ссылка на постер к фильму',
+    },
+  },
+  trailerLink: {
+    type: String,
+    required: true,
+    validate: {
+      validator(trailerLink) {
+        return validator.isURL(trailerLink);
+      },
+      message: 'Неверно указана ссылка на трейлер фильма',
+    },
+  },
+  thumbnail: {
+    type: String,
+    required: true,
+    validate: {
+      validator(thumbnail) {
+        return validator.isURL(thumbnail);
+      },
+      message: 'Неверно указана ссылка на трейлер фильма',
+    },
+  },
+  owner: {
+    type: mongoose.Types.ObjectId,
+    ref: 'user',
+    required: true,
+  },
+  movieId: {
+    type: mongoose.Types.ObjectId,
+    // ref: '',
+    required: true,
+  },
+  nameRU: {
+    type: String,
+    required: true,
+    validate: {
+      function(nameRU) {
+        return /[a-яa-z0-9\-\s]/gi.test(nameRU);
+      },
+      message: 'Название содержит недопустимые для фильма',
+    },
+  },
+  nameEN: {
+    type: String,
+    required: true,
+    validate: {
+      function(nameEN) {
+        return /[a-z0-9\-\s]/gi.test(nameEN);
+      },
+      message: 'Неверно указана ссылка на трейлер фильма',
+    },
+  },
+});
+
+module.exports = mongoose.model('movie', movieSchema);

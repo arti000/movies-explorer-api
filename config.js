8d536acfdf3eba947f299b01beccf7138c7da791
Artem Kireev <al-mysri@yandex.ru>
2022-08-12 17:43:13 +0300

    fix: dev production

--- config.js
+++ config.js
@@ -6,12 +6,10 @@ const {
   PORT = 3000,
   MONGO_DB = 'mongodb://localhost:27017/moviesdb',
   JWT_SECRET = 'jwt_secret',
-  NODE_ENV = 'production',
 } = process.env;
 
 module.exports = {
   PORT,
   MONGO_DB,
   JWT_SECRET,
-  NODE_ENV,
 };
